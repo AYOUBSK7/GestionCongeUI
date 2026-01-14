b7f29712846334cd5ccd7978d68947b2015eabd8
ChahredineBACCOUCH <chahredine.baccouche@gmail.com>
2026-01-14 12:53:26 +0100

    Modifier EntrepriseListView.vue

--- src/views/admin/entreprises/EntrepriseListView.vue
+++ src/views/admin/entreprises/EntrepriseListView.vue
@@ -18,16 +18,7 @@
             ></path>
           </svg>
         </button>
-        <router-link to="/admin/dashboard" class="p-1 hover:bg-azure-hover rounded">
-          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            ></path>
-          </svg>
-        </router-link>
+
         <span class="font-semibold tracking-tight">RH Platform</span>
         <span class="text-azure-300">|</span>
         <span class="text-sm">Gestion des Entreprises</span>
